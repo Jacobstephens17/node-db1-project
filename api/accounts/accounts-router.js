58e6ca10aad2421f99cade8af7c058274d54160f
Jacob Stephens <jacobstephens2@gmail.com>
2021-02-15 17:10:51 -0800

    changed database back to development -_-

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const router = express.Router();
 const Account = require("./accounts-model");
+const router = express.Router();
 
 
 router.get("/", async (req, res) => {
--- api/server.js
+++ api/server.js
@@ -2,12 +2,11 @@ const express = require("express");
 
 const AccountRouter = require("./accounts/accounts-router");
 
-const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
-server.use("/api/accounts", AccountRouter);
+server.use("/api/accounts",AccountRouter);
 
 server.get("/", (req, res) => {
     res.status(200).json({ message: "Working" })
